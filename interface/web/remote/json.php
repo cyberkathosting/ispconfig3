073273c8a1a04cd1213d6c88d4ada01223122129
mcramer <m.cramer@pixcept.de>
2013-09-03 16:14:51 +0000

    - Implemented json remote handler, example:   http://yourispconfig:8080/remote/json.php?sites_web_domain_get   with POST data:   session=12345678901234567890123456789012&data=%7B%22type%22%3A%22vhost%22%2C%22active%22%3A%22y%22%2C%22php%22%3A%22fast-cgi%22%2C%22fastcgi_php_version%22%3A%22%22%2C%22%23LIMIT%23%22%3A1%7D   where data is an urlencoded json-object of the params array used in SOAP api.

---
+++ interface/web/remote/json.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('../../lib/config.inc.php');
+$conf['start_session'] = false;
+require_once('../../lib/app.inc.php');
+
+$app->load('json_handler');
+$json_handler = new ISPConfigJSONHandler();
+$json_handler->run();
+
+?>
