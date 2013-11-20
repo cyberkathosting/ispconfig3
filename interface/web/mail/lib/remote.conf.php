d7e78a89dca5a4a6cc9665a43c7e0aaf9931f577
Marius Cramer <m.cramer@pixcept.de>
2013-11-20 16:07:10 +0100

    Added FS#3228 - remoting: add mail_relay_recipient functions

--- interface/web/mail/lib/remote.conf.php
+++ interface/web/mail/lib/remote.conf.php
@@ -8,6 +8,7 @@ $function_list['mail_alias_get,mail_alias_add,mail_alias_update,mail_alias_delet
 $function_list['mail_forward_get,mail_forward_add,mail_forward_update,mail_forward_delete'] = 'Mail forward functions';
 $function_list['mail_catchall_get,mail_catchall_add,mail_catchall_update,mail_catchall_delete'] = 'Mail catchall functions';
 $function_list['mail_transport_get,mail_transport_add,mail_transport_update,mail_transport_delete'] = 'Mail transport functions';
+$function_list['mail_relay_get,mail_relay_add,mail_relay_update,mail_relay_delete'] = 'Mail relay functions';
 $function_list['mail_whitelist_get,mail_whitelist_add,mail_whitelist_update,mail_whitelist_delete'] = 'Mail whitelist functions';
 $function_list['mail_blacklist_get,mail_blacklist_add,mail_blacklist_update,mail_blacklist_delete'] = 'Mail blacklist functions';
 $function_list['mail_spamfilter_user_get,mail_spamfilter_user_add,mail_spamfilter_user_update,mail_spamfilter_user_delete'] = 'Mail spamfilter user functions';
