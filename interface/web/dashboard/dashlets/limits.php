779b174e3d72667b12d5815f314f31beafde4710
Dominik Mueller <info@profi-webdesign.net>
2014-01-21 20:11:29 +0100

    missing bracket added

--- interface/web/dashboard/dashlets/limits.php
+++ interface/web/dashboard/dashlets/limits.php
@@ -71,7 +71,7 @@ class dashlet_limits {
 
 		$limits[] = array('field' => 'limit_web_aliasdomain',
 			'db_table' => 'web_domain',
-						  'db_where' => "(type = 'alias' OR type = 'vhostalias'");
+						  'db_where' => "(type = 'alias' OR type = 'vhostalias')");
 
 		$limits[] = array('field' => 'limit_ftp_user',
 			'db_table' => 'ftp_user',
