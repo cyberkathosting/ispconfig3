ac7ef7851be9ac53fec519848fe1b3cedde22521
Till Brehm <tbrehm@ispconfig.org>
2014-03-28 10:35:52 +0100

    Fixed default value for backup_interval of mail_users.

--- install/sql/ispconfig3.sql
+++ install/sql/ispconfig3.sql
@@ -918,7 +918,7 @@ CREATE TABLE `mail_user` (
   `disablelmtp` enum('n','y') NOT NULL default 'n',
   `disabledoveadm` enum('n','y') NOT NULL default 'n',
   `last_quota_notification` date NULL default NULL,
-  `backup_interval` VARCHAR( 255 ) NOT NULL,
+  `backup_interval` VARCHAR( 255 ) NOT NULL default 'none',
   `backup_copies` INT NOT NULL DEFAULT '1',
   PRIMARY KEY  (`mailuser_id`),
   KEY `server_id` (`server_id`,`email`),
