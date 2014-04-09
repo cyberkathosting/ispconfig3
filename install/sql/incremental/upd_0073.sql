ea82a03050626b7a37037ce95a4e418acc2a2af4
Dominik <info@profi-webdesign.net>
2014-04-09 19:41:26 +0200

    changed default values

--- install/sql/incremental/upd_0073.sql
+++ install/sql/incremental/upd_0073.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `client_template` ADD `limit_backup` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_webdav_user`;
-ALTER TABLE `client` ADD `limit_backup` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'n' AFTER `limit_webdav_user`;
+ALTER TABLE `client_template` ADD `limit_backup` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'y' AFTER `limit_webdav_user`;
+ALTER TABLE `client` ADD `limit_backup` ENUM( 'n', 'y' ) NOT NULL DEFAULT 'y' AFTER `limit_webdav_user`;
