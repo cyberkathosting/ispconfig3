d50cada50e92a788468cd97757a2dbc5d2cfb87c
Till Brehm <tbrehm@ispconfig.org>
2014-04-24 16:00:21 +0200

    Removed static dbispconfig database name in sql patch file.

--- install/sql/incremental/upd_0068.sql
+++ install/sql/incremental/upd_0068.sql
@@ -1,4 +1,4 @@
-ALTER TABLE  `dbispconfig`.`web_domain` ADD UNIQUE  `serverdomain` (  `server_id` ,  `domain` );
+ALTER TABLE  `web_domain` ADD UNIQUE  `serverdomain` (  `server_id` ,  `domain` );
 
 ALTER TABLE  `dns_rr` DROP KEY rr,
 	CHANGE  `name`  `name` VARCHAR( 128 ) NOT NULL,
