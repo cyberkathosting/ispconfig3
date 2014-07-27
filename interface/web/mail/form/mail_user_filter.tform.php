95ae83a3ca7603981842571902cde18088879500
Till Brehm <tbrehm@ispconfig.org>
2014-07-27 16:07:58 +0200

    Fixed: FS#2811 - Creating a mail filter ends with Soap Error: target_error_regex

--- interface/web/mail/form/mail_user_filter.tform.php
+++ interface/web/mail/form/mail_user_filter.tform.php
@@ -112,7 +112,7 @@ $form["tabs"]['filter'] = array (
 			'datatype' => 'VARCHAR',
 			'formtype' => 'TEXT',
 			'validators' => array (  0 => array ( 'type' => 'REGEX',
-					'regex' => '/^[\p{Latin}0-9\.\-\_\ ]{0,100}$/u',
+					'regex' => '/^[\p{Latin}0-9\.\-\_\ \&]{0,100}$/u',
 					'errmsg'=> 'target_error_regex'),
 			),
 			'default' => '',
