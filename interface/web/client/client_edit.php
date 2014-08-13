babacd5d8283c54a3b5dd740c327428b7440d9f4
Sergio Cambra <sergio@programatica.es>
2014-08-13 13:40:53 +0200

    checkbox to convert client on reseller, fixes FS#925

--- interface/web/client/client_edit.php
+++ interface/web/client/client_edit.php
@@ -96,6 +96,10 @@ class page_action extends tform_actions {
 		//* Resellers shall not be able to create another reseller
 		if($_SESSION["s"]["user"]["typ"] == 'user') {
 			$this->dataRecord['limit_client'] = 0;
+		} else {
+			if($this->dataRecord["reseller"]) {
+				$this->dataRecord["limit_client"] = 1;
+			}
 		}
 
 		if($this->id != 0) {
