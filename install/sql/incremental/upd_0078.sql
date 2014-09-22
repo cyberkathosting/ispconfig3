4e2d727aacc238b86b91bad012f8f42c8c47d31d
Florian Schaal <florian@schaal-24.de>
2014-09-22 15:06:10 +0200

    corrected upd_0078.sql

--- install/sql/incremental/upd_0078.sql
+++ install/sql/incremental/upd_0078.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `dns_rr` CHANGE `data` `data` TEXT NOT NULL DEFAULT ''
-ALTER TABLE `ftp_user` ADD `expires` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `dl_bandwidth` ;
+ALTER TABLE `dns_rr` CHANGE `data` `data` TEXT NOT NULL DEFAULT '';
