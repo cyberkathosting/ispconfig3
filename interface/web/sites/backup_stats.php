0c1fecfdf0db2be953c11d70521817d0fda76df4
Patrick Anders <p.anders@timmehosting.de>
2014-12-08 16:49:11 +0100

    backup_stats: add links  & add backup counter

--- interface/web/sites/backup_stats.php
+++ interface/web/sites/backup_stats.php
@@ -17,12 +17,18 @@ class list_action extends listform_actions {
 
 	public function prepareDataRow($rec)
 	{
+		global $app;
+
 		$rec = parent::prepareDataRow($rec);
 
 		$rec['active'] = "<div id=\"ir-Yes\" class=\"swap\"><span>Yes</span></div>";
 		if ($rec['backup_interval'] === 'none') {
-			$rec['active'] = "<div class=\"swap\" id=\"ir-No\"><span>No</span></div>";
+			$rec['active']        = "<div class=\"swap\" id=\"ir-No\"><span>No</span></div>";
+			$rec['backup_copies'] = 0;
 		}
+
+		$recBackup = $app->db->queryOneRecord('SELECT COUNT(backup_id) AS backup_count FROM web_backup WHERE parent_domain_id = ?', $rec['domain_id']);
+		$rec['backup_copies_exists'] = $recBackup['backup_count'];
 
 		return $rec;
 	}
