610315b8b6fd82e878d910e034304e1439461d60
Florian Schaal <florian@schaal-24.de>
2015-01-22 07:43:01 +0100

    increase size for txt-records for the admin to change txt-records for dkim

--- interface/web/dns/form/dns_txt.tform.php
+++ interface/web/dns/form/dns_txt.tform.php
@@ -174,5 +174,6 @@ if($_SESSION["s"]["user"]["typ"] == 'admin') {
 	unset($form["tabs"]['dns']['fields']['data']['validators']);
 	$form["tabs"]['dns']['fields']['data']['validators'][0]['type'] = 'NOTEMPTY';
 	$form["tabs"]['dns']['fields']['data']['validators'][0]['errmsg'] = 'data_error_empty';
+	$form["tabs"]['dns']['fields']['data']['maxlength'] = 512;
 }
 ?>
