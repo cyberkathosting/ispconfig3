881871012e82096cdb0df1a530c9dbbababca021
Florian Schaal <florian@schaal-24.de>
2015-08-23 12:31:34 +0200

    fixed typo

--- interface/web/admin/form/server_ip_map.tform.php
+++ interface/web/admin/form/server_ip_map.tform.php
@@ -28,7 +28,7 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-$form["title"]    = "IPv4 Addresse mapping";
+$form["title"]    = "IPv4 Address mapping";
 $form["description"]  = "Form to map IPv4-addresses for Web-Server";
 $form["name"]    = "server_ip_map";
 $form["action"]   = "server_ip_map_edit.php";
