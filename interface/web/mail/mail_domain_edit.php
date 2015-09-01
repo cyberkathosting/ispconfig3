ae9ca85a82c4001b1e6861bb3c91067812a210b7
Till Brehm <tbrehm@ispconfig.org>
2015-09-01 12:19:48 +0200

    Fixed: FS#3982 - Cannot modify mail domain when owner

--- interface/web/mail/mail_domain_edit.php
+++ interface/web/mail/mail_domain_edit.php
@@ -332,7 +332,7 @@ class page_action extends tform_actions {
 		} else {
 			//* We do not allow users to change a domain which has been created by the admin
 			$rec = $app->db->queryOneRecord("SELECT domain from mail_domain WHERE domain_id = ?", $this->id);
-			if($rec['domain'] != $this->dataRecord["domain"] && $app->tform->checkPerm($this->id, 'u')) {
+			if($rec['domain'] != $this->dataRecord["domain"] && !$app->tform->checkPerm($this->id, 'u')) {
 				//* Add a error message and switch back to old server
 				$app->tform->errorMessage .= $app->lng('The Domain can not be changed. Please ask your Administrator if you want to change the domain name.');
 				$this->dataRecord["domain"] = $rec['domain'];
