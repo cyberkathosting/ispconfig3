c0a85582a1e6ffd0b86fba4466981ba9c3ede0fe
Florian Schaal <florian@schaal-24.de>
2015-11-06 16:14:12 +0100

    FS#4024 - can't run ispconfig_update.sh to switch to dev

--- server/scripts/update_from_dev.sh
+++ server/scripts/update_from_dev.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 cd /tmp
+rm -rf ispconfig3-master*
 wget -O ispconfig3-dev.tar.gz "http://git.ispconfig.org/ispconfig/ispconfig3/repository/archive.tar.gz?ref=master"
 tar xzf ispconfig3-dev.tar.gz
-cd ispconfig3.git/install
+cd ispconfig3-master*/install
 php -q \
     -d disable_classes= \
     -d disable_functions= \
     -d open_basedir= \
     update.php
 cd /tmp
-rm -rf /tmp/ispconfig3.git /tmp/ispconfig3-dev.tar.gz
+rm -rf /tmp/ispconfig3-master* /tmp/ispconfig3-dev.tar.gz
 
 exit 0
