bdd7348bc339889d7a8878b4b3bad55f43e4f945
Marius Cramer <m.cramer@pixcept.de>
2015-12-08 17:31:37 +0100

    - fixed language entries for letsencrypt

--- server/plugins-available/apache2_plugin.inc.php
+++ server/plugins-available/apache2_plugin.inc.php
@@ -1153,8 +1153,7 @@ class apache2_plugin {
 					$app->system->chmod($webroot . "/.well-known/acme-challenge", "g+s");
 					
 					if(file_exists("/root/.local/share/letsencrypt/bin/letsencrypt")) {
-						print "/root/.local/share/letsencrypt/bin/letsencrypt auth --text --agree-tos --authenticator=webroot --server=https://acme-v01.api.letsencrypt.org/directory --rsa-key-size=4096 --email postmaster@$domain --domains $lddomain --webroot-path " . escapeshellarg($webroot) . "\n";
-						$this->_exec("/root/.local/share/letsencrypt/bin/letsencrypt auth --text --agree-tos --authenticator=webroot --server=https://acme-v01.api.letsencrypt.org/directory --rsa-key-size=4096 --email postmaster@$domain --domains $lddomain --webroot-path " . escapeshellarg($webroot));
+						$this->_exec("/root/.local/share/letsencrypt/bin/letsencrypt auth --text --agree-tos --authenticator webroot --server https://acme-v01.api.letsencrypt.org/directory --rsa-key-size 4096 --email postmaster@$domain --domains $lddomain --webroot-path " . escapeshellarg($webroot));
 					}
 				};
 
