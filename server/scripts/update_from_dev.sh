0b76df438470977010b2548082237febc7991448
Jesse Norell <jesse@kci.net>
2015-12-23 14:22:57 -0700

    use mktemp in update scripts to avoid race conditions

--- server/scripts/update_from_dev.sh
+++ server/scripts/update_from_dev.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-cd /tmp
-rm -f ispconfig3-dev.tar.gz
+{
+    umask 0077 \
+    && tmpdir=`mktemp -dt "$(basename $0).XXXXXXXXXX"` \
+    && test -d "${tmpdir}" \
+    && cd "${tmpdir}"
+} || {
+    echo 'mktemp failed'
+    exit 1
+}
+
 wget -O ispconfig3-dev.tar.gz "http://git.ispconfig.org/ispconfig/ispconfig3/repository/archive.tar.gz?ref=master"
-rm -rf ispconfig3-master*
 tar xzf ispconfig3-dev.tar.gz
+
+echo -n "Latest git version:  "
+ls -1d ispconfig3-master*
 cd ispconfig3-master*/install
+
 php -q \
     -d disable_classes= \
     -d disable_functions= \
     -d open_basedir= \
     update.php
+
 cd /tmp
-rm -rf /tmp/ispconfig3-master* /tmp/ispconfig3-dev.tar.gz
+rm -rf "${tmpdir}"
 
 exit 0
