ffedcb9a80e8a3ff375993fc349d9ce969a1a4d7
Till Brehm <tbrehm@ispconfig.org>
2016-06-16 16:41:37 +0200

    First part of fix for issue #3896 - Unknown column 'default_mailserver' in 'field list' when saving client limits main template

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -8,3 +8,8 @@ UPDATE client SET mail_servers = default_mailserver WHERE (mail_servers = '' OR 
 UPDATE client SET xmpp_servers = default_xmppserver WHERE (xmpp_servers = '' OR xmpp_servers IS NULL);
 UPDATE client SET db_servers = default_dbserver WHERE (db_servers = '' OR db_servers IS NULL);
 UPDATE client SET dns_servers = default_dnsserver WHERE (dns_servers = '' OR dns_servers IS NULL);
+ALTER TABLE `client_template` ADD `default_slave_dnsserver` INT NOT NULL DEFAULT '0' AFTER `limit_dns_slave_zone`;
+ALTER TABLE `client_template` ADD `mail_servers` TEXT NULL DEFAULT NULL AFTER `template_type`;
+ALTER TABLE `client_template` ADD `web_servers` TEXT NULL DEFAULT NULL AFTER `limit_xmpp_httparchive`;
+ALTER TABLE `client_template` ADD `dns_servers` TEXT NULL DEFAULT NULL AFTER `limit_aps`;
+ALTER TABLE `client_template` ADD `db_servers` TEXT NULL DEFAULT NULL AFTER `limit_dns_record`;
