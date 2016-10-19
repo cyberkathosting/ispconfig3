8c8e9bec7dc3147fcbe070e25c09b53dc989d24a
Till Brehm <tbrehm@ispconfig.org>
2016-10-19 18:15:41 +0200

    Fixed #4250 Enabling firewall does not link plugin.

--- server/plugins-available/firewall_plugin.inc.php
+++ server/plugins-available/firewall_plugin.inc.php
@@ -38,7 +38,7 @@ class firewall_plugin {
 	public function onInstall() {
 		global $conf;
 
-		if($conf['bastille']['installed'] == true && $conf['services']['firewall'] == true) {
+		if(($conf['bastille']['installed'] == true || $conf['ufw']['installed'] == true || $conf['firewall']['installed'] == true) && $conf['services']['firewall'] == true) {
 			return true;
 		} else {
 			return false;
