5a945f3a7ef5b182e3f335db4f2e2b208533d03f
Marius Burkard <m.burkard@pixcept.de>
2016-10-25 09:27:41 +0200

    - fixed mysql command for backup restore

--- server/plugins-available/backup_plugin.inc.php
+++ server/plugins-available/backup_plugin.inc.php
@@ -129,7 +129,7 @@ class backup_plugin {
 						//$db_name = $parts[1];
 						preg_match('@^db_(.+)_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}\.sql\.gz$@', $backup['filename'], $matches);
 						$db_name = $matches[1];
-						$command = "gunzip --stdout ".escapeshellarg($backup_dir.'/'.$backup['filename'])." | mysql -h '".escapeshellarg($clientdb_host)."' -u '".escapeshellarg($clientdb_user)."' -p'".escapeshellarg($clientdb_password)."' '".$db_name."'";
+						$command = "gunzip --stdout ".escapeshellarg($backup_dir.'/'.$backup['filename'])." | mysql -h ".escapeshellarg($clientdb_host)." -u ".escapeshellarg($clientdb_user)." -p".escapeshellarg($clientdb_password)." ".escapeshellarg($db_name);
 						exec($command);
 					}
 					unset($clientdb_host);
