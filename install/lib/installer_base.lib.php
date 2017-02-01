1d5cbdb89d101056e12be0298bd57394b1ecbbb0
ncomputers.org <contact@ncomputers.org>
2017-01-31 23:00:37 -0600

    added support for newer versions of mariadb-server

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -227,7 +227,7 @@ class installer_base {
 		//* check sql-mode
 		$check_sql_mode = $this->db->queryOneRecord("SELECT @@sql_mode");
 
-		if ($check_sql_mode['@@sql_mode'] != '' && $check_sql_mode['@@sql_mode'] != 'NO_ENGINE_SUBSTITUTION') {
+		if(!strpos($check_sql_mode['@@sql_mode'],'NO_ENGINE_SUBSTITUTION')) {
 			echo "Wrong SQL-mode. You should use NO_ENGINE_SUBSTITUTION. Add\n\n";
 			echo "    sql-mode=\"NO_ENGINE_SUBSTITUTION\"\n\n";
 			echo"to the mysqld-section in your mysql-config and restart mysqld afterwards\n";
