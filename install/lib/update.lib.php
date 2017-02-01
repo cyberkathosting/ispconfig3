7f91b4300e99e72eac2874e7a4e254091885d2d7
ncomputers.org <contact@ncomputers.org>
2017-02-01 14:44:54 -0600

    Florian Schaal request

--- install/lib/update.lib.php
+++ install/lib/update.lib.php
@@ -128,7 +128,7 @@ function updateDbAndIni() {
 	if($check_sql_mode['@@sql_mode']!='' && strpos($check_sql_mode['@@sql_mode'],'NO_ENGINE_SUBSTITUTION')===false) {
 		echo "Wrong SQL-mode. You should use NO_ENGINE_SUBSTITUTION. Add\n\n";
 		echo "    sql-mode=\"NO_ENGINE_SUBSTITUTION\"\n\n";
-		echo"to the mysqld-section in your mysql-config and restart mysqld afterwards\n";
+		echo"to the mysqld-section in your mysql-config on this server\n";
 		die();
 	}
 
