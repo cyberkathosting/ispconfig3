e313d006f6b7ea0d26bc9c1c11643c6b79b36a3f
Till Brehm <tbrehm@ispconfig.org>
2017-06-20 15:06:39 +0200

    Fixed: #4674 Permission on ftp_traffic table after update to 3.1.2

--- install/lib/installer_base.lib.php
+++ install/lib/installer_base.lib.php
@@ -672,6 +672,14 @@ class installer_base {
 				if(!$this->dbmaster->query($query, $value['db'] . '.dns_soa', $value['user'], $host)) {
 					$this->warning('Unable to set rights of user in master database: '.$value['db']."\n Query: ".$query."\n Error: ".$this->dbmaster->errorMessage);
 				}
+				
+				$query = "GRANT SELECT, INSERT, UPDATE ON ?? TO ?@?";
+				if ($verbose){
+					echo $query ."\n";
+				}
+				if(!$this->dbmaster->query($query, $value['db'] . '.ftp_traffic', $value['user'], $host)) {
+					$this->warning('Unable to set rights of user in master database: '.$value['db']."\n Query: ".$query."\n Error: ".$this->dbmaster->errorMessage);
+				}
 
 			}
 
