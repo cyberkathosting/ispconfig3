4baefe24066cd3f3c1f522998a24dc2c2de695c8
Till Brehm <tbrehm@ispconfig.org>
2017-07-12 16:02:11 +0200

    Added a server migration mode setting which will be used to prevent that LE tries to recreate certs during a server migration.

--- interface/web/admin/form/server_config.tform.php
+++ interface/web/admin/form/server_config.tform.php
@@ -363,6 +363,12 @@ $form["tabs"]['server'] = array(
 			'default' => 'y',
 			'value' => array(0 => 'n', 1 => 'y')
 		),
+		'migration_mode' => array(
+			'datatype' => 'VARCHAR',
+			'formtype' => 'CHECKBOX',
+			'default' => 'n',
+			'value' => array(0 => 'n', 1 => 'y')
+		),
 		//#################################
 		// ENDE Datatable fields
 		//#################################
