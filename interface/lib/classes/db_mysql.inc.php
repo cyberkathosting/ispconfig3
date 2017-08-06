cb52d1b1151e71efe27c17b8ff295b4173e9b8dd
Marius Burkard <m.burkard@pixcept.de>
2017-08-06 19:18:24 +0200

    - fixed PHP warnings about class constuctors (mentioned in #4750) fixes #4752

--- interface/lib/classes/db_mysql.inc.php
+++ interface/lib/classes/db_mysql.inc.php
@@ -1031,7 +1031,7 @@ class db_result {
 	 *
 	 * @access private
 	 */
-	public function db_result($iResId, $iConnection) {
+	public function __construct($iResId, $iConnection) {
 		$this->_iResId = $iResId;
 		$this->_iConnection = $iConnection;
 	}
@@ -1157,7 +1157,7 @@ class fakedb_result {
 	 *
 	 * @access private
 	 */
-	public function fakedb_result($aData) {
+	public function __construct($aData) {
 		$this->aResultData = $aData;
 		$this->aLimitedData = $aData;
 		reset($this->aLimitedData);
