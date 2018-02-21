2add46b984a93260e79ce7903f843106f0139808
Pascal Dreissen <pdreissen@notes-it.nl>
2018-02-21 16:15:40 +0100

    Fix sorting bug e-mail backup stats

--- interface/web/mail/backup_stats.php
+++ interface/web/mail/backup_stats.php
@@ -21,16 +21,19 @@ class list_action extends listform_actions {
 		$app->uses('functions');
 
 		$rec = parent::prepareDataRow($rec);
-
-		$rec['active'] = "Yes";
-		if ($rec['backup_interval'] === 'none') {
-			$rec['active']        = "No";
+		//var_dump($rec);
+		$rec['active'] = $app->lng('yes_txt');
+		if ($rec['backup_interval'] === 'none' || $rec['backup_interval'] === '') {
+			$rec['backup_interval'] = strtolower($app->lng('None'));
+			$rec['active'] = $app->lng('no_txt');
 			$rec['backup_copies'] = 0;
 		}
+		$rec['interval_sort'] = $rec['type'] . $rec['backup_interval'];
 		$recBackup = $app->db->queryOneRecord('SELECT COUNT(backup_id) AS backup_count FROM mail_backup WHERE mailuser_id = ?', $rec['mailuser_id']);
 		$rec['backup_copies_exists'] = $recBackup['backup_count'];
 		unset($recBackup);
 		$recBackup = $app->db->queryOneRecord('SELECT SUM(filesize) AS backup_size FROM mail_backup WHERE mailuser_id = ?', $rec['mailuser_id']);
+		$rec['backup_size_sort'] = $recBackup['backup_size'];
 		$rec['backup_size'] = $app->functions->formatBytes($recBackup['backup_size']);
 
 		return $rec;
