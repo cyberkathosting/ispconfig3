67d81d3f6d367b8ce011b5bf85c566fdb815c6ce
Till Brehm <tbrehm@ispconfig.org>
2018-07-16 17:31:29 +0200

    Fixed nginx socket path in apps vhost for Ubuntu 18.04.

--- server/plugins-available/apps_vhost_plugin.inc.php
+++ server/plugins-available/apps_vhost_plugin.inc.php
@@ -183,6 +183,11 @@ class apps_vhost_plugin {
 			}
 			$content = str_replace('{use_tcp}', $use_tcp, $content);
 			$content = str_replace('{use_socket}', $use_socket, $content);
+			
+			// Fix socket path on PHP 7 systems
+			if(file_exists('/var/run/php/php7.0-fpm.sock'))	$content = str_replace('/var/run/php5-fpm.sock', '/var/run/php/php7.0-fpm.sock', $content);
+			if(file_exists('/var/run/php/php7.1-fpm.sock'))	$content = str_replace('/var/run/php5-fpm.sock', '/var/run/php/php7.1-fpm.sock', $content);
+			if(file_exists('/var/run/php/php7.2-fpm.sock'))	$content = str_replace('/var/run/php5-fpm.sock', '/var/run/php/php7.2-fpm.sock', $content);
 
 			// PHP-FPM
 			// Dont just copy over the php-fpm pool template but add some custom settings
