d74f0addd498ba75ff7cac5e39fbbb4d72930c3a
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2018-11-05 17:27:59 +0100

    Update update.php to fix the condition for non-webservers to check for ispserver.crt file instead of whether apache or nginx is installed since apache might be installed together with mailman.

--- install/update.php
+++ install/update.php
@@ -524,11 +524,10 @@ if ($inst->install_ispconfig_interface) {
 	}
 }
 
-// Create SSL certs for non-webserver(s) ?
-if(($conf['apache']['installed'] && $conf['apache']['installed']) == false) {
-    if(strtolower($inst->simple_query('Do you want to create SSL certs for your server?', array('y', 'n'), 'y')) == 'y') {
+// Create SSL certs for non-webserver(s)?
+if(!file_exists(/usr/local/ispconfig/interface/ssl/ispserver.crt)) {
+    if(strtolower($inst->simple_query('Do you want to create SSL certs for your server?', array('y', 'n'), 'y')) == 'y')
         $inst->make_ispconfig_ssl_cert();
-    }
 }
 
 $inst->install_ispconfig();
