f2124f238a97809dd9a13d1557013f58053c59e0
Marius Burkard <m.burkard@pixcept.de>
2018-11-16 16:39:36 +0100

    - added some logging

--- server/lib/classes/ispconfig_addon_installer_base.inc.php
+++ server/lib/classes/ispconfig_addon_installer_base.inc.php
@@ -36,6 +36,7 @@ class ispconfig_addon_installer_base {
 	protected function copyInterfaceFiles() {
 		global $conf;
 		
+		$app->log('Copying interface files.', 0, false);
 		$install_dir = realpath($conf['rootpath'] . '/..');
 		
 		if(is_dir($this->temp_dir . '/interface')) {
@@ -44,6 +45,7 @@ class ispconfig_addon_installer_base {
 			$command = 'cp -rf ' . escapeshellarg($this->temp_dir . '/interface') . ' ' . escapeshellarg($install_dir . '/');
 			exec($command, $ret, $retval);
 			if($retval != 0) {
+				$app->log('Copying interface files failed.', 2, false);
 				throw new AddonInstallerException('Command ' . $command . ' failed with code ' . $retval);
 			}
 			
@@ -56,6 +58,8 @@ class ispconfig_addon_installer_base {
 	protected function copyServerFiles() {
 		global $conf;
 		
+		$app->log('Copying server files.', 0, false);
+		
 		$install_dir = realpath($conf['rootpath'] . '/..');
 		
 		if(is_dir($this->temp_dir . '/server')) {
@@ -64,6 +68,7 @@ class ispconfig_addon_installer_base {
 			$command = 'cp -rf ' . escapeshellarg($this->temp_dir . '/server'). ' ' . escapeshellarg($install_dir . '/');
 			exec($command, $ret, $retval);
 			if($retval != 0) {
+				$app->log('Copying interface files failed.', 2, false);
 				throw new AddonInstallerException('Command ' . $command . ' failed with code ' . $retval);
 			}
 			return true;
@@ -75,9 +80,12 @@ class ispconfig_addon_installer_base {
 	protected function copyAddonFiles() {
 		global $app, $conf;
 		
+		$app->log('Copying addon files.', 0, false);
+		
 		$install_dir = realpath($conf['rootpath'] . '/..') . '/addons/' . $this->addon_ident;
 		if(!is_dir($install_dir)) {
 			if(!$app->system->mkdir($install_dir, false, 0750, true)) {
+				$app->log('Addons dir missing and could not be created.', 2, false);
 				throw new AddonInstallerException('Could not create addons dir ' . $install_dir);
 			}
 		}
@@ -89,6 +97,7 @@ class ispconfig_addon_installer_base {
 			exec($command, $ret, $retval);
 			if($retval != 0) {
 				/* TODO: logging */
+				$app->log('Warning or error on copying addon files. Returncode of command ' . $command . ' was: ' . $retval .'.', 0, false);
 			}
 			
 			return true;
@@ -100,6 +109,7 @@ class ispconfig_addon_installer_base {
 	protected function executeSqlStatements() {
 		global $app, $conf;
 		
+		$app->log('Adding addon entry to db.', 0, false);
 		// create addon entry if not existing
 		$qry = 'INSERT IGNORE INTO `addons` (`addon_ident`, `addon_version`, `addon_name`, `db_version`) VALUES (?, ?, ?, ?)';
 		$app->db->query($qry, $this->addon_ident, $this->addon_version, $this->addon_name, 0);
@@ -110,6 +120,7 @@ class ispconfig_addon_installer_base {
 			$incremental = 0;
 			if($check['db_version']) {
 				$incremental = $check['db_version'];
+				$app->log('Current db version is ' . $incremental . '.', 0, false);
 			}
 		}
 		
@@ -121,9 +132,11 @@ class ispconfig_addon_installer_base {
 			if(is_file($sql_file)) {
 				$ret = null;
 				$retval = 0;
+				$app->log('Loading ' . $sql_file . ' into db.', 0, false);
 				exec($mysql_command . ' < ' . escapeshellarg($sql_file), $ret, $retval);
 				if($retval != 0) {
 					/* TODO: log error! */
+					$app->log('Loading ' . $sql_file . ' into db failed.', 1, false);
 				}
 			}
 		} else {
@@ -135,9 +148,11 @@ class ispconfig_addon_installer_base {
 				} else {
 					$ret = null;
 					$retval = 0;
+					$app->log('Loading ' . $sql_file . ' into db.', 0, false);
 					exec($mysql_command . ' < ' . escapeshellarg($sql_file), $ret, $retval);
 					if($retval != 0) {
 						/* TODO: log error! */
+						$app->log('Loading ' . $sql_file . ' into db failed.', 1, false);
 					}
 				}
 				
@@ -145,6 +160,7 @@ class ispconfig_addon_installer_base {
 			}
 			
 			$app->db->query('UPDATE `addons` SET `addon_version` = ?, `db_version` = ? WHERE `addon_ident` = ?', $this->addon_version, $new_db_version, $this->addon_ident);
+			$app->log('Db version of addon is now ' . $new_db_version . '.', 0, false);
 		}
 		
 		return true;
@@ -153,10 +169,12 @@ class ispconfig_addon_installer_base {
 	public function onBeforeInstall() { }
 	
 	public function onInstall() {
+		$app->log('Running onInstall()', 0, false);
 		$this->copyAddonFiles();
 		$this->copyInterfaceFiles();
 		$this->copyServerFiles();
 		$this->executeSqlStatements();
+		$app->log('Finished onInstall()', 0, false);
 	}
 	
 	public function onAfterInstall() { }
@@ -164,10 +182,12 @@ class ispconfig_addon_installer_base {
 	public function onBeforeUpdate() { }
 	
 	public function onUpdate() {
+		$app->log('Running onUpdate()', 0, false);
 		$this->copyAddonFiles();
 		$this->copyInterfaceFiles();
 		$this->copyServerFiles();
 		$this->executeSqlStatements();
+		$app->log('Finished onUpdate()', 0, false);
 	}
 	
 	public function onAfterUpdate() { }
