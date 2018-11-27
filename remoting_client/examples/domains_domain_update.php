e5ac9179520c1e51c99179e5f92c8e5fa667435e
Herman van Rink <rink@initfour.nl>
2018-11-27 10:54:27 +0100

    Add example for domains_domain_update

---
+++ remoting_client/examples/domains_domain_update.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'soap_config.php';
+
+
+$client = new SoapClient(null, array('location' => $soap_location,
+		'uri'      => $soap_uri,
+		'trace' => 1,
+		'exceptions' => 1));
+
+
+try {
+	if($session_id = $client->login($username, $password)) {
+		echo 'Logged successfull. Session ID:'.$session_id.'<br />';
+	}
+
+	//* Set the function parameters.
+	$client_id = 1;
+	$primary_id = 42; // The domain_id
+	$params = array(
+		'domain' => 'cellar.door'
+	);
+
+	$domain_id = $client->domains_domain_update($session_id, $client_id, $primary_id, $params);
+
+	echo "Domain ID: ".$domain_id."<br>";
+
+	if($client->logout($session_id)) {
+		echo 'Logged out.<br />';
+	}
+
+
+} catch (SoapFault $e) {
+	echo $client->__getLastResponse();
+	die('SOAP Error: '.$e->getMessage());
+}
+
+?>
