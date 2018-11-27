c42fed03e0b209ec3e5ae0d0324a5f8ce53744af
Herman van Rink <rink@initfour.nl>
2018-11-27 10:53:35 +0100

    correct parameters

--- interface/lib/classes/remote.d/domains.inc.php
+++ interface/lib/classes/remote.d/domains.inc.php
@@ -66,13 +66,13 @@ class remoting_domains extends remoting {
 	}
 
 	//* Update a record
-	public function domains_domain_update($session_id, $client_id, $params)
+	public function domains_domain_update($session_id, $client_id, $primary_id, $params)
 	{
 		if(!$this->checkPerm($session_id, 'domains_domain_update')) {
 			throw new SoapFault('permission_denied', 'You do not have the permissions to access this function.');
 			return false;
 		}
-		return $this->updateQuery('../client/form/domain.tform.php', $client_id, $params);
+		return $this->updateQuery('../client/form/domain.tform.php', $client_id, $primary_id, $params);
 	}
 
 	//* Delete a record
