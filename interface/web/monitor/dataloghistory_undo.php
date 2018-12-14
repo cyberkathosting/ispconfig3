fcfeb80627ba486e48dc659a445ab9bf6473c1a5
Marius Burkard <m.burkard@pixcept.de>
2018-12-14 16:25:13 +0100

    - added datalog undelete to viewer

--- interface/web/monitor/dataloghistory_undo.php
+++ interface/web/monitor/dataloghistory_undo.php
@@ -50,18 +50,36 @@ $dbidx = explode(':', $record['dbidx']);
 
 $old_record = $app->db->queryOneRecord('SELECT * FROM ?? WHERE ??=?', $record['dbtable'], $dbidx[0], $dbidx[1]);
 
-if (is_array($old_record)) {
-	if(!$data = unserialize(stripslashes($record['data']))) {
-		$data = unserialize($record['data']);
+if($record['action'] === 'u') {
+	if (is_array($old_record)) {
+		if(!$data = unserialize(stripslashes($record['data']))) {
+			$data = unserialize($record['data']);
+		}
+
+		$new_record = $data['old'];
+
+		$app->db->datalogUpdate($record['dbtable'], $new_record, $dbidx[0], $dbidx[1]);
+
+		$app->tpl->setVar('success', true);
+	} else {
+		$app->tpl->setVar('success', false);
 	}
+} elseif($record['action'] === 'd') {
+	if(is_array($old_record)) {
+		$app->tpl->setVar('success', false);
+		$app->tpl->setVar('error_txt', $wb['error_undelete_txt']);
+	} else {
+		if(!$data = unserialize(stripslashes($record['data']))) {
+			$data = unserialize($record['data']);
+		}
 
-	$new_record = $data['old'];
+		$new_record = $data['old'];
+		/* TODO: maybe check some data, e. g. server_id -> server still there?, sys_groupid -> sys_group/sys_user still there? */
 
-	$app->db->datalogUpdate($record['dbtable'], $new_record, $dbidx[0], $dbidx[1]);
+		$app->db->datalogInsert($record['dbtable'], $new_record, $dbidx[0]);
 
-	$app->tpl->setVar('success', true);
-} else {
-	$app->tpl->setVar('success', false);
+		$app->tpl->setVar('success', true);
+	}
 }
 
 $app->tpl_defaults();
