44d34be61eb7f9c99c1504bf8640d31dd1cb30ec
Florian Schaal <florian@schaal-24.de>
2019-02-26 13:50:30 +0100

    DNS: xfer / also_notify fields too short (Fixes #5237)

--- install/sql/ispconfig3.sql
+++ install/sql/ispconfig3.sql
@@ -620,7 +620,7 @@ CREATE TABLE `dns_soa` (
   `minimum` int(11) unsigned NOT NULL default '3600',
   `ttl` int(11) unsigned NOT NULL default '3600',
   `active` enum('N','Y') NOT NULL DEFAULT 'N',
-  `xfer` varchar(255) NOT NULL DEFAULT '',
+  `xfer` TEXT NOT NULL DEFAULT '',
   `also_notify` varchar(255) default NULL,
   `update_acl` varchar(255) default NULL,
   `dnssec_initialized` ENUM('Y','N') NOT NULL DEFAULT 'N',
