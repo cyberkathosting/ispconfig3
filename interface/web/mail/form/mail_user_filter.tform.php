d77b1219c5d1a7968501c135551b8f3887ca2dcc
Herman van Rink <rink@initfour.nl>
2019-06-20 15:42:26 +0200

    remove old commented code

--- interface/web/mail/form/mail_user_filter.tform.php
+++ interface/web/mail/form/mail_user_filter.tform.php
@@ -94,7 +94,6 @@ $form["tabs"]['filter'] = array (
 			'datatype' => 'VARCHAR',
 			'formtype' => 'SELECT',
 			'default' => '',
-			//'value'  => array('contains'=>'contains_txt','is' => 'Is','begins'=>'Begins with','ends'=>'Ends with')
 			'value'  => array('contains'=>'contains_txt', 'is' => 'is_txt', 'begins'=>'begins_with_txt', 'ends'=>'ends_with_txt')
 		),
 		'searchterm' => array (
