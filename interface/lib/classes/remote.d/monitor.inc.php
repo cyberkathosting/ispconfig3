e470fde84a904409aa7912bedcf424d6467d9e0f
Marius Burkard <m.burkard@pixcept.de>
2019-09-02 15:06:44 +0200

    Remote API: monitor_jobqueue_count returning wrong count in multiserver setup, fixes #5382

--- interface/lib/classes/remote.d/monitor.inc.php
+++ interface/lib/classes/remote.d/monitor.inc.php
@@ -39,7 +39,6 @@ class remoting_monitor extends remoting {
 
 		if(!$this->checkPerm($session_id, 'monitor_jobqueue_count')) {
 			throw new SoapFault('permission_denied', 'You do not have the permissions to access this function.');
-			return false;
 		}
 		
 		$server_id = intval($server_id);
@@ -48,7 +47,7 @@ class remoting_monitor extends remoting {
 			$servers = $app->db->queryAllRecords("SELECT server_id, updated FROM server");
 			$sql = 'SELECT count(datalog_id) as jobqueue_count FROM sys_datalog WHERE ';
 			foreach($servers as $sv) {
-				$sql .= " (datalog_id > ".$sv['updated']." AND server_id = ".$sv['server_id'].") OR ";
+				$sql .= " (datalog_id > ".$sv['updated']." AND server_id IN (0,".$sv['server_id'].")) OR ";
 			}
 			$sql = substr($sql, 0, -4);
 			$tmp = $app->db->queryOneRecord($sql);
@@ -56,7 +55,7 @@ class remoting_monitor extends remoting {
 			
 		} else {
 			$server = $app->db->queryOneRecord("SELECT updated FROM server WHERE server_id = ?",$server_id);
-			$tmp = $app->db->queryOneRecord('SELECT count(datalog_id) as jobqueue_count FROM sys_datalog WHERE datalog_id > ?',$server['updated']);
+			$tmp = $app->db->queryOneRecord('SELECT count(datalog_id) as jobqueue_count FROM sys_datalog WHERE datalog_id > ? AND server_id IN ?',$server['updated'], array(0, $server_id));
 			return $tmp['jobqueue_count'];
 		}
 	}
