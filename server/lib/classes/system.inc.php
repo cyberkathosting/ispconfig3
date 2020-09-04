7a830e4978107119516fbbad43584535b5c7f824
Michael Seevogel <git@michaelseevogel.de>
2020-09-04 13:12:31 +0200

    updated move code

--- server/lib/classes/system.inc.php
+++ server/lib/classes/system.inc.php
@@ -935,10 +935,18 @@ class system{
 	}
 
         function move($file1, $file2) {
-		return copy($file1, $file2);
 		if(file_exists($file1) || is_link($file1)) {
-                        return unlink($file1);
-                }
+			return copy($file1, $file2);
+	                if(file_exists($file2) || is_link($file2)) {
+				return unlink($file1);
+			} else {
+				$app->log("move failed: couldn't move file/link " .$file1." to ".$file2, LOGLEVEL_DEBUG);
+				return false;
+			}
+		} else {
+			$app->log("move failed: " .$file1." doesn't exist.", LOGLEVEL_DEBUG);
+			return false;
+		}
         }
 
 	function touch($file, $allow_symlink = false){
