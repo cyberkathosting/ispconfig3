6af96c6ba7631cc612c14c45c18add5fef4c3291
Till Brehm <tbrehm@ispconfig.org>
2020-09-18 15:20:47 +0200

    Moved upd_dev_collection.sql content to upd_0090.sql file in preparation of ISPConfig 3.2beta2 release.

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,5 +1,1 @@
-ALTER TABLE `web_domain` ADD  `jailkit_chroot_app_sections` mediumtext NULL DEFAULT NULL;
-ALTER TABLE `web_domain` ADD  `jailkit_chroot_app_programs` mediumtext NULL DEFAULT NULL;
-ALTER TABLE `web_domain` ADD  `delete_unused_jailkit` enum('n','y') NOT NULL DEFAULT 'n';
-ALTER TABLE `web_domain` ADD  `last_jailkit_update` date NULL DEFAULT NULL;
-ALTER TABLE `web_domain` ADD  `last_jailkit_hash` varchar(255) DEFAULT NULL;
+
