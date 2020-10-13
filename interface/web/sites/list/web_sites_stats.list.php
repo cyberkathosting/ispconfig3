5ccd7f22da3caf105909ee8ce729e313be1a2b6b
Pascal Dreissen <pdreissen@notes-it.nl>
2020-10-13 10:08:30 +0200

    Backport !744 Dashboard enhancements / quota view enhancements using bootstrap progressbars

--- interface/web/sites/list/web_sites_stats.list.php
+++ interface/web/sites/list/web_sites_stats.list.php
@@ -43,7 +43,7 @@ $liste["paging_tpl"]  = "templates/paging.tpl.htm";
 $liste["auth"]    = "yes";
 
 // mark columns for php sorting (no real mySQL columns)
-$liste["phpsort"] = array('this_month', 'last_month', 'this_year', 'last_year');
+$liste["phpsort"] = array('this_month', 'last_month', 'this_year', 'last_year', 'percentage', 'quota_sort');
 
 /*****************************************************
 * Suchfelder
