96cc8c082cc05ae476011398157af706fff3cad3
Marius Burkard <m.burkard@pixcept.de>
2020-11-18 09:16:40 +0100

    - also change mail_user to row format dynamic and add column

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,6 +1,7 @@
 
 -- we need those to fix some installations failing in 0089 and 0090
 ALTER TABLE `web_domain` ROW_FORMAT=DYNAMIC;
+ALTER TABLE `mail_user` ROW_FORMAT=DYNAMIC;
 ALTER IGNORE TABLE `web_domain` ADD COLUMN `proxy_protocol` ENUM('n','y') NOT NULL DEFAULT 'n' AFTER `log_retention`;
 ALTER IGNORE TABLE `web_domain` ADD  `backup_format_web` VARCHAR( 255 ) NOT NULL default 'default' AFTER `backup_copies`;
 ALTER IGNORE TABLE `web_domain` ADD  `backup_format_db` VARCHAR( 255 ) NOT NULL default 'gzip' AFTER `backup_format_web`;
@@ -16,6 +17,7 @@ ALTER IGNORE TABLE `web_domain` CHANGE `apache_directives` `apache_directives` m
 ALTER IGNORE TABLE `web_domain` CHANGE `nginx_directives` `nginx_directives` mediumtext NULL DEFAULT NULL;
 UPDATE `web_domain` as w LEFT JOIN sys_group as g ON (g.groupid = w.sys_groupid) INNER JOIN `server_php` as p ON (w.fastcgi_php_version = CONCAT(p.name, ':', p.php_fastcgi_binary, ':', p.php_fastcgi_ini_dir) AND p.server_id IN (0, w.server_id) AND p.client_id IN (0, g.client_id)) SET w.server_php_id = p.server_php_id, w.fastcgi_php_version = '' WHERE w.server_php_id = 0;
 UPDATE `web_domain` as w LEFT JOIN sys_group as g ON (g.groupid = w.sys_groupid) INNER JOIN `server_php` as p ON (w.fastcgi_php_version = CONCAT(p.name, ':', p.php_fpm_init_script, ':', p.php_fpm_ini_dir, ':', p.php_fpm_pool_dir) AND p.server_id IN (0, w.server_id) AND p.client_id IN (0, g.client_id)) SET w.server_php_id = p.server_php_id, w.fastcgi_php_version = '' WHERE w.server_php_id = 0;
+ALTER IGNORE TABLE `mail_user` ADD `forward_in_lda` enum('n','y') NOT NULL default 'n' AFTER `cc`;
 -- end of fixes
 
 -- drop old php column because new installations don't have them (fails in multi-server)
