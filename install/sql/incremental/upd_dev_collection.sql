b6698e0852bcba04839052e831c90ff3cedec817
Jesse Norell <jesse@kci.net>
2021-08-19 15:55:10 -0600

    mail_forwarding source must be unique

--- install/sql/incremental/upd_dev_collection.sql
+++ install/sql/incremental/upd_dev_collection.sql
@@ -1,2 +1,6 @@
 -- default spamfilter_users.policy_id to 0
 ALTER TABLE `spamfilter_users` ALTER `policy_id` SET DEFAULT 0;
+
+-- mail_forwarding.source must be unique
+ALTER TABLE `mail_forwarding` DROP KEY `server_id`;
+ALTER TABLE `mail_forwarding` ADD UNIQUE KEY `server_id` (`server_id`, `source`);
